820a43ce656c875ccaf9b1b0ee010e2936460378
Kari Vigna <karivigna24@gmail.com>
2023-06-15 11:51:05 -0700

    javascript execution attempts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     let form = document.querySelector("form");
     form.onsubmit = function(event) {
-        
+        event.preventDefault();
 
         let ans1 = document.getElementById("ans1");
         ans1.setAttritube("class", "hidden");
@@ -14,13 +14,30 @@ window.onload = function () {
         function handleRadio(event) {
             event.preventDefault();
             const Q1 = document.querySelector("input[name=q1]:checked").value;
+            const Q2 = document.querySelector("input[name=q2]:checked").value;
+            const Q3 = document.querySelector("input[name=q3]:checked").value;
+            const Q4 = document.querySelector("input[name=q4]:checked").value;
+            const Q5 = document.querySelector("input[name=q5]:checked").value;
         }
+        
         window.addEventListener("load", function() {
             this.document.getElementById("radio-form").addEventListener("submit", handleRadio);
         });
+        let result;
+            if (q1 === yes, q2 === yes, q3 === yes, q4 ===yes, q5 === yes) {
+                ans1.removeAttribute("class", "hidden");
+            }else if (q1 === no, q2 === no, q3 === no, q4 ===no, q5 === no) {
+                ans2.removeAttribute("class", "hidden")
+            }else {
+                ans3.removeAttribute("class", "hidden");
+            }
+
+            }
+            }
+
+        
 
 
-        event.preventDefault();
+       
+        
 
-    }
-}
