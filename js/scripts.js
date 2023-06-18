cbc9a70b82edc6ff8fa87cfd42bc88f42529462b
Kari Vigna <karivigna24@gmail.com>
2023-06-17 17:32:05 -0700

    further javascript tinkering, and trial and error

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,33 @@
 window.onload = function () {
     let form = document.querySelector("form");
     form.onsubmit = function(event) {
-        return 
-       
+   
+            let Q1 = document.querySelector("input[name=q1]:checked").value;
+            let Q2 = document.querySelector("input[name=q2]:checked").value;
+            let Q3 = document.querySelector("input[name=q3]:checked").value;
+            let Q4 = document.querySelector("input[name=q4]:checked").value;
+            let Q5 = document.querySelector("input[name=q5]:checked").value;
+        }  
 
-        let ans1 = document.getElementById("ans1");
-        ans1.setAttritube("class", "hidden");
-        let ans2 = document.getElementById("ans2");
-        ans2.setAttribute("class", "hidden");
-        let ans3 = document.getElementById("ans3");
-        ans3.setAttribute("class", "hidden");
+        //let ans1 = document.getElementById("ans1");
+        //ans1.setAttritube("class", "hidden");
+        //let ans2 = document.getElementById("ans2");
+        //ans2.setAttribute("class", "hidden");
+        //let ans3 = document.getElementById("ans3");
+        //ans3.setAttribute("class", "hidden");
         
-
-        function handleRadio(event) {
-            event.preventDefault();
-            const Q1 = document.querySelector("input[name=q1]:checked").value;
-            const Q2 = document.querySelector("input[name=q2]:checked").value;
-            const Q3 = document.querySelector("input[name=q3]:checked").value;
-            const Q4 = document.querySelector("input[name=q4]:checked").value;
-            const Q5 = document.querySelector("input[name=q5]:checked").value;
-        }
-        
-        window.addEventListener("load", function() {
-            this.document.getElementById("radio-form").addEventListener("submit", handleRadio);
         
 
         let result;
             if (q1 === yes, q2 === yes, q3 === yes, q4 ===yes, q5 === yes) {
-                ans1.removeAttribute("class", "hidden");
+                document.querySelector(div#ans1).removeAttribute("class", "hidden");
             } else if (q1 === no, q2 === no, q3 === no, q4 ===no, q5 === no) {
-                ans2.removeAttribute("class", "hidden")
+                document.querySelector(div#ans2).removeAttribute("class", "hidden")
             } else {
-                ans3.removeAttribute("class", "hidden");
+                document.querySelector(div#ans3).removeAttribute("class", "hidden");
             }
             }
-            )}};
+            };
 
         
 
