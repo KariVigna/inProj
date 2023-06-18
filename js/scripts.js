e6030256868770aa95d4ebe48eb3024d63f10f19
Kari Vigna <karivigna24@gmail.com>
2023-06-17 18:51:18 -0700

    further css styling and javascript attempts

--- js/scripts.js
+++ js/scripts.js
@@ -9,25 +9,27 @@ window.onload = function () {
             let Q5 = document.querySelector("input[name=q5]:checked").value;
         
 
-        //let ans1 = document.getElementById("ans1");
-        //ans1.setAttritube("class", "hidden");
-        //let ans2 = document.getElementById("ans2");
-        //ans2.setAttribute("class", "hidden");
-        //let ans3 = document.getElementById("ans3");
-        //ans3.setAttribute("class", "hidden");
+        let ans1 = document.getElementById("ans1");
+        ans1.setAttritube("class", "hidden");
+        let ans2 = document.getElementById("ans2");
+        ans2.setAttribute("class", "hidden");
+        let ans3 = document.getElementById("ans3");
+        ans3.setAttribute("class", "hidden");
         
         
 
         let result;
-            if (q1 === yes, q2 === yes, q3 === yes, q4 ===yes, q5 === yes) {
+            if (Q1 === yes, Q2 === yes, Q3 === yes, Q4 ===yes, Q5 === yes) {
                 document.querySelector(div#ans1).removeAttribute("class");
             } else if (q1 === no, q2 === no, q3 === no, q4 ===no, q5 === no) {
                 document.querySelector(div#ans2).removeAttribute("class")
             } else {
                 document.querySelector(div#ans3).removeAttribute("class");
             }
-            }};
-            ;
+
+            event.preventDefault();
+            };
+        };
 
         
 
