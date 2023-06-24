15fb0bac01f2d136856879adad3a19b4cac9ee51
Kari Vigna <karivigna24@gmail.com>
2023-06-24 15:39:11 -0700

    improved syntax/typos, form now working

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 window.onload = function () {
-    let form = document.querySelector("radio-form");
-    form.addEventListener = ("submit", function(event) {
+    console.log("window loads");
+    let form = document.querySelector("form");
+    form.onsubmit = function(event) {
         event.preventDefault();
+        console.log("form submits");
    
     let Q1 = document.querySelector("input[name=q1]:checked").value;
     let Q2 = document.querySelector("input[name=q2]:checked").value;
@@ -9,26 +11,25 @@ window.onload = function () {
     let Q4 = document.querySelector("input[name=q4]:checked").value;
     let Q5 = document.querySelector("input[name=q5]:checked").value;
         
-    let ans1 = document.getElementById("ans1");
-    ans1.setAttritube("class", "hidden");
-    let ans2 = document.getElementById("ans2");
-    ans2.setAttribute("class", "hidden");
-    let ans3 = document.getElementById("ans3");
-    ans3.setAttribute("class", "hidden");
+    // let ans1 = document.getElementById("ans1");
+    // ans1.setAttritube("class", "hidden");
+    // let ans2 = document.getElementById("ans2");
+    // ans2.setAttribute("class", "hidden");
+    // let ans3 = document.getElementById("ans3");
+    // ans3.setAttribute("class", "hidden");
 
         
-        let result;
-            if (Q1 === "yes", Q2 === "yes", Q3 === "yes", Q4 === "yes", Q5 === "yes") {
-                document.querySelector("div#ans1").removeAttribute("class");
-            } else if (Q1 === "no", Q2 === "no", Q3 === "no", Q4 === "no", Q5 === "no") {
-                document.querySelector("div#ans2").removeAttribute("class")
-            } else {
-                document.querySelector("div#ans3").removeAttribute("class");
-            }
+    let result;
+        if (Q1 === "yes", Q2 === "yes", Q3 === "yes", Q4 === "yes", Q5 === "yes") {
+            document.querySelector("div#ans1").removeAttribute("class");
+        } else if (Q1 === "no", Q2 === "no", Q3 === "no", Q4 === "no", Q5 === "no") {
+            document.querySelector("div#ans2").removeAttribute("class");
+        } else {
+            document.querySelector("div#ans3").removeAttribute("class");
+        }
             //event.preventDefault();
-            });
         };
-
+    };
         
 
 
